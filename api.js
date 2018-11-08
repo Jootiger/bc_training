39c280da0a839f092d42c73c6984a51800db0f31
jootiger <neivlee@gmail.com>
2018-11-08 17:46:34 +0900

    blockchain.js -> merkle tree add, api.js -> mining(add merkletree)

--- api.js
+++ api.js
@@ -1,17 +1,57 @@
 var express = require('express');
 var app = express();
 
+var Blockchain = require('./blockchain');
+var jscoin = new Blockchain(); //제네시스 블록 생성
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+
 // http://localhost:3000/ 요청이 들어왔을 때 동작하는 기능
 app.get('/', function(req, res){
     res.send("Hellow World");
 });
 
 app.get('/blockchain', function(req, res){
-    res.send("Blockchain start");
+    res.send(jscoin);
 });
 
 app.post('/transaction', function(req, res){
-    res.send("Transaction Start");
+    console.log(req.body);
+    jscoin.createNewTransaction(req.body.sender, req.body.recipient, req.body.amount);
+    res.send(jscoin);
 });
 
+app.get('/mining', function(req,res){
+    //마지막 블록을 추출 -> 지금 만들려고 하는 블록의 이전 블록
+    const lastBlock = jscoin.getLastBlock();
+
+    //마지막 블록의 해쉬를 추출
+    const previousBlockHash = lastBlock['hash'];
+    
+    //대기 중인 트랜잭션을 가져와서 새로 만들 블록에 데이터로 추가
+    const currentBlockData = {
+        index: lastBlock['index'] + 1 ,
+        transactions: jscoin.pendingTransactions        
+    }
+    //작업증명을 통해서 nonce값을 계산
+    const nonce = jscoin.pow(previousBlockHash, currentBlockData);
+
+    //블록해쉬를 계산
+    const blockHash = jscoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    //머클루트 계산
+    const merkleRoot = jscoin.calculateMerkleRoot(currentBlockData['transactions']);
+
+    //새블록을 생성
+    const newBlock = jscoin.createNewBlock(previousBlockHash, nonce, blockHash, merkleRoot);
+
+    //결과를 반환(msg, 새블록)
+    res.json(
+        {
+            msg: currentBlockData['index'] + 'block created.',
+            block: newBlock
+        }
+    );
+});
 app.listen(3000);
--- blockchain.js
+++ blockchain.js
@@ -2,6 +2,9 @@
 //import sha256
 const sha256 = require('sha256');
 
+//Merkle 모듈 임포트
+const merkle = require('merkle');
+
 // 블록체인 객체를 정의
 function Blockchain(){
     //블록체인
@@ -14,7 +17,7 @@ function Blockchain(){
     this.difficulty = 1;
 
     //제네시스 블록을 생성
-    this.createNewBlock(0, 0, 0); 
+    this.createNewBlock(0, 0, 0, 0); 
 }
 
 // 파라미터로 전달된 해쉬가 difficulty에 설정된 개수만큼 0으로 시작하고 있는지 체크
@@ -27,33 +30,47 @@ Blockchain.prototype.checkDifficulty = function(hash){
 // 블록의 해쉬를 구하는 함수를 추가
 Blockchain.prototype.hashBlock 
     = function(previousBlockHash, currentBlockData, nonce){
-        const data = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        const merkleRoot = this.calculateMerkleRoot(currentBlockData['transactions']);
+        //const data = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        const data = previousBlockHash + nonce.toString() + merkleRoot;
         const hash = sha256(data);    
         return hash;
 };
 
 // 작업증명(PoW)을 수행하는 함수를 추가
-Blockchain.prototype.pow 
-    = function(previousBlockHash, currentBlockData){
-        let nonce = 0;
+Blockchain.prototype.pow = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    //while(hash.substring(0, 5) != this.difficulty){
+    while(!this.checkDifficulty(hash)){
+        nonce++;
         let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-        //while(hash.substring(0, 5) != this.difficulty){
-        while(!this.checkDifficulty(hash)){
-            nonce++;
-            let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-        }
-        return nonce;
+    }
+    return nonce;
+};
+//트랜잭션 정보를 이용해서 머클루트를 계산해서 반환하는 함수
+Blockchain.prototype.calculateMerkleRoot = function(transactions){
+    // JSON 타입에 트랜잭션 정보를 문자열 배열 형태로 변환한 값
+    var strArrayTxs = [];
+    transactions.forEach(tx => {
+        strArrayTxs.push(JSON.stringify(tx));
+    });
+
+    // 머클트리 생성
+    var tree = merkle('sha256').sync(strArrayTxs);
+    return tree.root();
 };
 
 //블록 생성 함수
-Blockchain.prototype.createNewBlock = function(previousBlockHash, nonce, hash){
+Blockchain.prototype.createNewBlock = function(previousBlockHash, nonce, hash, merkleRoot){
     const newBlock ={
         index: this.chain.length +1,
         timestamp: Date.now(),
-        transactions: this.pendingTransactions,
+        merkleRoot: merkleRoot,
         nonce: nonce,
         hash: hash,
-        previousBlockhash: previousBlockHash
+        previousBlockhash: previousBlockHash,
+        transactions: this.pendingTransactions,
     }
 
     this.pendingTransactions = [];  
@@ -69,11 +86,11 @@ Blockchain.prototype.getLastBlock = function(){
 
 
 //Add function to transaction
-Blockchain.prototype.createNewTransaction = function(_sender, _recipeint, _amount){
+Blockchain.prototype.createNewTransaction = function(_sender, _recipient, _amount){
     //Constroctur of Transactions
     const newTransaction = {
         sender: _sender,
-        recipient: _recipeint,
+        recipient: _recipient,
         amount: _amount
     };
 
