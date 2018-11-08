4d6cd490fe1c74e1a24557198c327b0b04ff2056
jootiger <neivlee@gmail.com>
2018-11-08 17:48:17 +0900

    blockchain.js -> merkle tree add, api.js -> mining(add merkletree)

--- test.js
+++ test.js
@@ -15,16 +15,23 @@ const currentBlockData = [
 //ca3bd0c3f57e94c7a8588b1391a176e11ec442998948c5fa20f7f71ddc4996e8
 //26f818534683440df377642e5fab72f51557a515eeafe9153b8382db0fff8970
 
-console.log(">>>>>" + jscoin.difficulty);
-let nonce = jscoin.pow(previousBlockHash, currentBlockData);
+// console.log(">>>>>" + jscoin.difficulty);
+// let nonce = jscoin.pow(previousBlockHash, currentBlockData);
 
-console.log(nonce);
+// console.log(nonce);
 
-console.log(jscoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// console.log(jscoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
-jscoin.difficulty = 2;
-console.log(">>>>>" + jscoin.difficulty);
-nonce = jscoin.pow(previousBlockHash, currentBlockData);
-console.log(nonce);
-console.log(jscoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// jscoin.difficulty = 2;
+// console.log(">>>>>" + jscoin.difficulty);
+// nonce = jscoin.pow(previousBlockHash, currentBlockData);
+// console.log(nonce);
+// console.log(jscoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
+const txs = [
+    {sender: 'aaa', recipient: 'bbb', amount: 100},
+    {sender: 'ccc', recipient: 'ddd', amount: 200},
+    {sender: 'eee', recipient: 'fff', amount: 400},
+];
+
+console.log(jscoin.calculateMerkleRoot(txs));
